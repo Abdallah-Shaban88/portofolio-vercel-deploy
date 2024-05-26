3ce3257bef6cba1931ceb59f8e1a67131e2854c1
Abdallah-Shaban88 <80660937+Abdallah-Shaban88@users.noreply.github.com>
2024-05-26 17:45:14 +0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Arima } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 const arima = Arima({subsets: ["latin"]})
@@ -19,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <body className={arima.className}>
+      <SpeedInsights/>
+      <Analytics/>
         {children}
         </body>
     </html>
