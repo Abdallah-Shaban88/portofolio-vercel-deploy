f9168d219b0d0de0023c5ca18e83e4e0b223ffa3
Abdallah-Shaban88 <80660937+Abdallah-Shaban88@users.noreply.github.com>
2024-05-27 06:43:54 -0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://hamada966533108772.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://hamada966533108772.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/blog',
+      url: 'https://hamada966533108772.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
